301252c047516d44c294cfbb33777af3a15a321d
Sean McAfee <eefacm@gmail.com>
2012-04-29 15:45:33 -0700

    Fixes cache setup.

--- MyRssBase.pm
+++ MyRssBase.pm
@@ -8,19 +8,24 @@ use constant ITEM_CACHE_MINUTES => 60 * 24 * 30;
 
 sub new {
     my $class = shift;
+
     my $self = $class->SUPER::new(
         scalar $class->FEED,
         undef,
         scalar $class->NAME,
         scalar $class->TITLE,
-        cache => {
-            dir   => "$ENV{HOME}/.rss-cache",
-            feed  => 60 * 5,
-            items => 60 * $class->ITEM_CACHE_MINUTES,
-        }
     );
+
+    $self->set_cache(
+        dir   => "$ENV{HOME}/.rss-cache",
+        feed  => 60 * 5,
+        items => 60 * $class->ITEM_CACHE_MINUTES,
+    );
+
     $self->init;
+
     return $self;
+
 }
 
 sub init {
