8fa2a52047d8f59c3b0586186449f0843f002f95
Sean McAfee <eefacm@gmail.com>
2012-08-14 23:22:38 -0700

    Detaches "related-keywords" div.

--- RegisterScience.pm
+++ RegisterScience.pm
@@ -13,7 +13,9 @@ use constant {
 sub render {
     my ($self, $item) = @_;
     my ($body) = $item->page->find('//div[@id="article"]');
-    $_->detach for $self->find($body, 'descendant::div[%s or %s]', 'ad-now', 'article-nav');
+    $_->detach for $self->find(
+        $body, 'descendant::div[%s or %s]', 'ad-now', 'article-nav', 'related-keywords'
+    );
     my ($tags) = $body->find('child::div[@id="tags"]');
     $tags->parent->splice_content($tags->pindex) if $tags;
     return $body;
