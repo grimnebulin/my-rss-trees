4e8e5727a7d6108256c6e0ba0c0488611047329b
Sean McAfee <eefacm@gmail.com>
2012-08-14 23:21:55 -0700

    Initial check-in of new Angry Video Game Nerd filter.

---
+++ AVGN.pm
@@ -0,0 +1,22 @@
+package AVGN;
+
+use base qw(RSS::Tree);
+use strict;
+
+use constant {
+    NAME  => 'avgn',
+    TITLE => 'AVGN',
+    FEED  => 'http://feeds2.feedburner.com/Cinemassacrecom',
+};
+
+
+sub render {
+    my ($self, $item) = @_;
+    $_->detach for $item->content->find(
+        '//p[descendant::a[contains(@href,"doubleclick")]]|//div[%s]', 'feedflare'
+    );
+    return $item->content;
+}
+
+
+1;
