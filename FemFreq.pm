3f4bea4cfdf8690c41f3a3562a3235ccb71b74d9
Sean McAfee <eefacm@gmail.com>
2013-09-16 14:49:10 -0700

    Xpath cleanup.

--- FemFreq.pm
+++ FemFreq.pm
@@ -12,7 +12,7 @@ use constant {
 
 sub render {
     my ($self, $item) = @_;
-    return $item->page->find('//div[%s]/p[not(attribute::*)]', 'entry');
+    return $item->page->find('//div[%s]/p[not(@*)]', 'entry');
 }
 
 
