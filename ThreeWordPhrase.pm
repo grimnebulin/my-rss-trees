be77afb404c514bd28ddbc26b78a8157246fea3b
Sean McAfee <eefacm@gmail.com>
2013-09-16 14:56:01 -0700

    Appends news (if any) to image.

--- ThreeWordPhrase.pm
+++ ThreeWordPhrase.pm
@@ -7,12 +7,15 @@ use constant {
     FEED  => 'http://www.threewordphrase.com/rss.xml',
     NAME  => '3word',
     TITLE => 'Three Word Phrase',
+    LIMIT => 3,
 };
 
 
 sub render {
     my ($self, $item) = @_;
-    return $item->page->find('//center/img');
+    my ($img)  = $item->page->find('//center/img') or return;
+    my ($text) = $img->findnodes('ancestor::table[1]/following-sibling::center');
+    return ($self->new_element('div', $img), $text ? $text->as_trimmed_text : ());
 }
 
 
