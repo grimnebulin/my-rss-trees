89b75d0a8744e8ba14e70b846ea6141b0ef6431f
Sean McAfee <eefacm@gmail.com>
2013-09-16 14:49:40 -0700

    Code cleanup.

--- Gameological.pm
+++ Gameological.pm
@@ -12,33 +12,21 @@ use constant {
 
 sub init {
     my $self = shift;
-    $self->add(Gameological::SawbuckGamer->new);
+    $self->add(
+        RSS::Tree::Node->new('sawbuck', 'Sawbuck Gamer')->match_title('Sawbuck Gamer')
+    );
 }
 
 sub render {
     my ($self, $item) = @_;
     my ($content) = $item->page->find('//div[@id="main-content"]')
         or return;
-    $_->detach for $content->findnodes(
-        'child::*[@id="page-category" or @id="share" or ' .
+    $self->remove(
+        $content,
+        '*[@id="page-category" or @id="share" or ' .
         '@id="outbrain" or @id="comments"]'
     );
     return $content;
-}
-
-{
-
-package Gameological::SawbuckGamer;
-
-use base qw(RSS::Tree::Node);
-
-sub new {
-    return shift->SUPER::new('sawbuck', 'Sawbuck Gamer')
-                ->match_title('Sawbuck Gamer');
-}
-
-*render = *Gameological::render;
-
 }
 
 
