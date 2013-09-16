845e226c26c6a6a2471742464ddea58b7bfdccef
Sean McAfee <eefacm@gmail.com>
2013-09-16 14:53:16 -0700

    Resets feed content: image, plus news if any.

--- QuestionableContent.pm
+++ QuestionableContent.pm
@@ -7,15 +7,14 @@ use constant {
     NAME  => 'qc',
     TITLE => 'Questionable Content',
     FEED  => 'http://www.questionablecontent.net/QCRSS.xml',
+    LIMIT => 10,
 };
 
 
 sub render {
     my ($self, $item) = @_;
-    my ($img) = $item->description->find('//img[contains(@src,"/comics/")]')
-        or return;
-    $img->parent->splice_content(0, $img->pindex) if $img->parent;
-    return;
+    my ($img) = $item->page->find('//div[@id="comic"]//img') or return;
+    return ($img, $item->page->find('//div[@id="news"]'));
 }
 
 
