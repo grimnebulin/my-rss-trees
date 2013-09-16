ff0653622c604f21ea9c488c6aef4ebac8fd4509
Sean McAfee <eefacm@gmail.com>
2013-09-16 14:42:44 -0700

    Pulls comic image from Dilber site.

--- Dilbert.pm
+++ Dilbert.pm
@@ -7,14 +7,27 @@ use constant {
     FEED  => 'http://feeds.feedburner.com/DilbertDailyStrip?format=xml',
     NAME  => 'dilbert',
     TITLE => 'Dilbert',
+    KEEP_GUID => 1,
 };
 
 
 sub render {
     my ($self, $item) = @_;
-    $_->detach for $item->description->find('//a[contains(@href,"doubleclick")]');
-    return $item->description;
+    my ($img) = $item->page->find('//div[%s]/img', 'STR_Image') or return;
+    $img->attr('onload', undef);  # Probably unnecessary.
+    return $img;
 }
+
+sub uri_for {
+    my ($self, $item) = @_;
+    return $item->guid;
+}
+
+sub decode_response {
+    my ($self, $response) = @_;
+    return $response->decoded_content(alt_charset => 'utf-8');
+}
+
 
 
 1;
