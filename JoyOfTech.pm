b24f0af68e223d6546c50e2795292a999355035a
Sean McAfee <eefacm@gmail.com>
2013-09-16 15:43:32 -0700

    Updates comic-fetching logic.

--- JoyOfTech.pm
+++ JoyOfTech.pm
@@ -15,9 +15,13 @@ sub render {
     my ($self, $item) = @_;
 
     my ($thumbnail) = $item->description->find('//a[img]') or return;
+    my $href = $thumbnail->attr('href');
 
-    if (my ($image) = $self->get($thumbnail->attr('href'))
-                           ->find('//img[contains(@src,"joyimages")]')) {
+    my $response = $self->agent->get($href);
+    $response->is_success or return;
+    my $page = $self->new_page($href, $response->decoded_content);
+
+    if (my ($image) = $page->find('//img[contains(@src,"/cartoons/")]')) {
         $thumbnail->postinsert($image->as_HTML);
         $thumbnail->replace_with($image);
     }
