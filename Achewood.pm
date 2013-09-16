07b69698b692549cb0760be34df819448754ad41
Sean McAfee <eefacm@gmail.com>
2013-09-16 14:37:40 -0700

    Appends comic image title below comic.

--- Achewood.pm
+++ Achewood.pm
@@ -4,15 +4,19 @@ use parent qw(RSS::Tree);
 use strict;
 
 use constant {
-    FEED  => 'http://www.achewood.com/rss.php',
-    NAME  => 'achewood',
-    TITLE => 'Achewood',
+    FEED    => 'http://www.achewood.com/rss.php',
+    NAME    => 'achewood',
+    TITLE   => 'Achewood',
+    KEEP_ENCLOSURE => 0,
 };
 
 
 sub render {
     my ($self, $item) = @_;
-    return $item->page->find('//img[%s]', 'comic')->shift;
+    my ($img) = $item->page->find('//img[%s]', 'comic') or return;
+    my ($title) = $img->attr('title', undef);
+    $title = $self->new_element('div', [ 'i', $title ]) if $title;
+    return ($img, $title ? $title : ());
 }
 
 
