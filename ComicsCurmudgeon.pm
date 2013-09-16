2d44a591619587d66a1ffdcf19d887027451c3df
Sean McAfee <eefacm@gmail.com>
2013-09-16 14:40:59 -0700

    Adds inline images to "Metapost" articles.

--- ComicsCurmudgeon.pm
+++ ComicsCurmudgeon.pm
@@ -9,11 +9,25 @@ use constant {
     TITLE => 'The Comics Curmudgeon',
 };
 
+
 sub render {
     my ($self, $item) = @_;
-    return $item->content->remove(
+
+    $item->content->remove(
         '//map|//*[self::p or self::table][descendant::img[contains(@src,"doubleclick") or contains(@src,"projectwonderful")]]'
     );
+
+    if ($item->title =~ /Metapost/) {
+        for my $link ($item->content->find('//p/a[contains(@href,"/images/")]')) {
+            $link->parent->postinsert(
+                $self->new_element('p', [ 'img', { src => $link->attr('href') } ])
+            );
+            $link->replace_with($self->new_element('b', $link->content_list));
+        }
+    }
+
+    return;
+
 }
 
 
