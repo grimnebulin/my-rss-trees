d1cb3bd9b7b0d16c19254c96a38c32741a5a9b60
Sean McAfee <eefacm@gmail.com>
2013-09-16 14:51:07 -0700

    Wraps first content node in a div; prepends item categories to feed content as pipe-delimited text.

--- Geekologie.pm
+++ Geekologie.pm
@@ -12,9 +12,26 @@ use constant {
 
 sub render {
     my ($self, $item) = @_;
-    return $item->description->remove(
+
+    $item->description->remove(
         '//p[descendant::a[contains(@href,"doubleclick")]]|//img[@height="1"]'
     );
+
+    my @guts = $item->description->guts;
+
+    if (@guts) {
+        $guts[0] = $self->new_element('div', $guts[0]);
+    }
+
+    if (my @categories = $item->categories) {
+        unshift @guts, $self->new_element(
+            'div', { style => 'font-size: smaller' }, join ' | ', @categories
+        );
+    }
+
+    return @guts;
+
 }
+
 
 1;
