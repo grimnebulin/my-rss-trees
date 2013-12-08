481f11a5bdd2241327d4010fcedd54cd0fd5fdbe
Sean McAfee <eefacm@gmail.com>
2013-12-08 12:41:50 -0800

    Code tweaking.

--- ComicsCurmudgeon.pm
+++ ComicsCurmudgeon.pm
@@ -14,15 +14,17 @@ sub render {
     my ($self, $item) = @_;
 
     $item->content->remove(
-        '//map|//*[self::p or self::table][descendant::img[contains(@src,"doubleclick") or contains(@src,"projectwonderful")]]'
+        '//map|//*[self::p or self::table][.//img[contains(@src,"doubleclick") or contains(@src,"projectwonderful") or contains(@src,"/images/ads/")]]'
     );
 
     if ($item->title =~ /Metapost/) {
-        for my $link ($item->content->find('//p/a[contains(@href,"/images/")]')) {
-            $link->parent->postinsert(
-                $self->new_element('p', [ 'img', { src => $link->attr('href') } ])
-            );
-            $link->replace_with($self->new_element('b', $link->content_list));
+        for my $p ($item->content->find('//p')) {
+            for my $link ($p->findnodes('.//a[contains(@href,"/images/")]')) {
+                $p->postinsert(
+                    $self->new_element('p', [ 'img', { src => $link->attr('href') } ])
+                );
+                $link->replace_with($self->new_element('b', $link->content_list));
+            }
         }
     }
 
