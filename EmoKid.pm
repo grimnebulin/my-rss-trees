7a9696ddaca3f60420dcb5251241e77f1b1832ee
Sean McAfee <eefacm@gmail.com>
2014-07-03 17:30:21 -0700

    Initial check-in.

---
+++ EmoKid.pm
@@ -0,0 +1,22 @@
+package EmoKid;
+
+use parent qw(RSS::Tree);
+use strict;
+
+use constant {
+    FEED  => 'http://www.cheerupemokid.com/feed',
+    NAME  => 'emokid',
+    TITLE => 'Emo Kid',
+};
+
+
+sub render {
+    my ($self, $item) = @_;
+    my ($img) = $item->page->find('//article[@id="comic"]/img')
+        or return;
+    my $title = $img->attr('alt');
+    return ($img, $title && $self->new_element('p', [ 'i', $title ]));
+}
+
+
+1;
