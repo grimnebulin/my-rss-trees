b51d871ff98c52ad79de709c20c2e3b3c278c14d
Sean McAfee <eefacm@gmail.com>
2014-12-20 12:17:36 -0500

    Add YouTube video durations when possible.

--- FailBlog.pm
+++ FailBlog.pm
@@ -1,5 +1,7 @@
 package FailBlog;
 
+use URI;
+use YouTubeInfo;
 use parent qw(RSS::Tree);
 use strict;
 
@@ -25,6 +27,8 @@ sub render {
     my ($self, $item) = @_;
 
     $item->content->remove('//script');
+    $item->content->remove('//p[.//img[contains(@src,"/completestore/")]]');
+    $item->content->remove('//div[count(*)=0]');
 
     if (my ($bound) = $item->content->find(
         '//div[.//a[contains(@href,"cheezburger.com/tag/")]]'
@@ -49,6 +53,48 @@ use parent qw(RSS::Tree::Node);
 sub test {
     my ($self, $item) = @_;
     return $item->content->find('//param|//iframe')->size > 0;
+}
+
+sub render {
+    my ($self, $item) = @_;
+
+    $self->root->render($item);
+
+    for my $iframe ($item->content->find('//iframe')) {
+        next if $iframe->attr('src') !~ m|youtube\.com/embed/(\w+)|;
+        my $duration = $item->cache->{$1} ||= do {
+            my $d = _get_duration($1);
+            defined $d ? $d : '?:??';
+        };
+        $iframe->replace_with(
+            $self->new_element(
+                'div', [ 'i', "Video duration: $duration" ],
+            ),
+            $self->new_element('div', $iframe),
+        );
+    }
+
+    return;
+
+}
+
+sub _get_duration {
+    my $vidid = shift;
+
+    defined(my $duration = YouTubeInfo::duration($vidid))
+        or return '??:??';
+
+    my $seconds = $duration % 60;
+    $duration = ($duration - $seconds) / 60;
+    my $minutes = $duration % 60;
+    $duration = ($duration - $minutes) / 60;
+    my $hours = $duration;
+
+    return sprintf '%d:%02d:%02d', $hours, $minutes, $seconds
+        if $hours > 0;
+
+    return sprintf '%02d:%02d', $minutes, $seconds;
+
 }
 
 }
