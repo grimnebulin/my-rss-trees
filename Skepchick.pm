2d274d3b45d3bf6d88a9ae44f6949541dba95e9c
Sean McAfee <eefacm@gmail.com>
2015-02-10 14:28:37 -0800

    Incorporate YouTube videos properly.

--- Skepchick.pm
+++ Skepchick.pm
@@ -14,5 +14,19 @@ sub init {
     shift->match_creator('watson');
 }
 
+sub render {
+    my ($self, $item) = @_;
+
+    my ($video) = $item->page->find(
+        '//article[%s]//div[iframe[contains(@src,"/www.youtube.com/")]]', 'post'
+    );
+
+    return (
+        $video,
+        $item->content->remove('//*[%s]', 'adsbygoogle')->guts,
+    );
+
+}
+
 
 1;
