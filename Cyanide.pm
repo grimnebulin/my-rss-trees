1f5deea646a2b3b29241270c63fcf495a8513051
Sean McAfee <eefacm@gmail.com>
2016-07-31 16:11:04 -0700

    Cyanide & Happiness tweaks

--- Cyanide.pm
+++ Cyanide.pm
@@ -15,14 +15,6 @@ use constant {
 sub render {
     my ($self, $item) = @_;
 
-    if ($item->title =~ /^short:/i
-        and my ($video) = $item->page->find('//div[@id="videoPlayer"]//iframe')) {
-        my $uri = URI->new($video->attr('src'));
-        $uri->query_param_delete('autoplay');
-        $video->attr('src', $uri);
-        return $video;
-    }
-
     my ($comic) = $item->page->find('//img[@id="main-comic"]');
     return $comic if $comic;
 
