c7af76ec7bdd1a28d9849c246f653fee0b1c37a5
Sean McAfee <eefacm@gmail.com>
2016-07-31 16:12:28 -0700

    IO9 tweaks

--- IO9.pm
+++ IO9.pm
@@ -1,12 +1,14 @@
 package IO9;
 
-use parent qw(RSS::Tree);
+use parent qw(MaybeLengthy);
 use strict;
 
 use constant {
     NAME  => 'io9',
     TITLE => 'IO9',
     FEED  => 'http://feeds.gawker.com/io9/full',
+    TEXT_LIMIT => 10000,
+    EMBED_LIMIT => 5,
 };
 
 
@@ -15,32 +17,41 @@ sub init {
     $self->add(RSS::Tree::Node->new('rbio9', 'TRv2')->match_creator('Bricken'));
 }
 
-sub render {
+sub get_body {
+    my ($self, $page) = @_;
+    my ($body) = $page->find('//div[%s]', 'post-content') or return;
+    $self->remove($body, '//*[%s or %s]|//aside', 'content-ad', 'hide');
+    $self->truncate($body, '//div[@id="inset_groups"]');
+    return $body;
+}
+
+sub abbreviate {
     my ($self, $item) = @_;
+    return $item->description->truncate('//img[@width="1" or @height="1"]');
+}
+
+sub postrender {
+    my ($self, $item, $body) = @_;
     my $creator = $self->new_element(
         'div', 'by ', [ 'i', $item->creator || 'Unknown' ]
     );
-    my $intro = $self->_intro($item) || $self->new_element(
-        'div', { style => 'border: 1px solid black' }, 'intro not available'
-    );
-    my ($body) = $item->page->find('//div[%s]', 'post-content') or return;
-    $self->remove($body, '//div[%s]|//aside', 'content-ad');
-    $self->truncate($body, '//div[@id="inset_groups"]');
-    return ($creator, $intro, $body);
+    return ($creator, $self->_intro($item), $body);
 }
 
 sub _intro {
     my ($self, $item) = @_;
 
-    if (my ($video) = $item->page->find('//article//iframe[contains(@src,"youtube.com") or contains(@src,"vimeo.com")]')) {
-        $video->attr('width', '500');
-        $video->attr('height', '400');
-        return $self->new_element('div', $video);
-    }
-
-    if (my ($img) = $item->page->find('//article//img[%s]', 'js_annotatable-image')) {
+    if (my ($img) = $item->page->find('//article//img[%s]', 'marquee')) {
         $img->attr($_, int($img->attr($_) * .5)) for qw(width height);
         return $img;
+    }
+
+    if (my ($video) = $item->page->find('//article//*[self::iframe or self::embed][contains(@src,"youtube.com") or contains(@src,"vimeo.com") or contains(@src,"ytimg.com")]')) {
+        if (0 == grep { $_->getValue eq $video->attr('src') } $item->description->find('//*[self::iframe or self::embed]/@src')) {
+            $video->attr('width', '500');
+            $video->attr('height', '400');
+            return $self->new_element('div', $video);
+        }
     }
 
     if (my ($img) = $item->description->find('p[img][count(*)=1]')) {
