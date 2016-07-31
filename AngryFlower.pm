afef5787339e342980c09cf88262f4ed1bf46f3a
Sean McAfee <eefacm@gmail.com>
2016-07-31 16:08:57 -0700

    AngryFlower tweaks

--- AngryFlower.pm
+++ AngryFlower.pm
@@ -7,21 +7,13 @@ use constant {
     FEED  => 'http://interglacial.com/rss/bob_the_angry_flower.rss',
     NAME  => 'angryflower',
     TITLE => 'Bob the Angry Flower',
+    LIMIT => 5,
 };
 
-my $TEXT_LIMIT = 1000;
 
 sub render {
     my ($self, $item) = @_;
-
-    for my $td ($item->page->find('//td')) {
-        if (length($td->as_text) >= $TEXT_LIMIT) {
-            return $td->content_list;
-        }
-    }
-
     return $item->page->find('//img[1]');
-
 }
 
 
