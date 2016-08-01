612b359e4c2a480ab727ff7fd5cca3e23342df0c
Sean McAfee <eefacm@gmail.com>
2016-07-31 17:09:24 -0700

    Failblog updates

--- FailBlog.pm
+++ FailBlog.pm
@@ -1,7 +1,6 @@
 package FailBlog;
 
 use URI;
-use YouTubeInfo;
 use parent qw(RSS::Tree);
 use strict;
 
@@ -26,7 +25,6 @@ sub test {
 sub render {
     my ($self, $item) = @_;
 
-    $item->content->remove('//script');
     $item->content->remove('//p[.//img[contains(@src,"/completestore/")]]');
     $item->content->remove('//div[count(*)=0]');
 
@@ -48,53 +46,11 @@ sub render {
 
 package FailBlogVideo;
 
-use parent qw(RSS::Tree::Node);
+use parent qw(RSS::Tree::Node Youtube);
 
 sub test {
     my ($self, $item) = @_;
     return $item->content->find('//param|//iframe')->size > 0;
-}
-
-sub render {
-    my ($self, $item) = @_;
-
-    $self->root->render($item);
-
-    for my $iframe ($item->content->find('//iframe')) {
-        next if $iframe->attr('src') !~ m|youtube\.com/embed/(\w+)|;
-        my $duration = $item->cache->{$1} ||= do {
-            my $d = _get_duration($1);
-            defined $d ? $d : '?:??';
-        };
-        $iframe->replace_with(
-            $self->new_element(
-                'div', [ 'i', "Video duration: $duration" ],
-            ),
-            $self->new_element('div', $iframe),
-        );
-    }
-
-    return;
-
-}
-
-sub _get_duration {
-    my $vidid = shift;
-
-    defined(my $duration = YouTubeInfo::duration($vidid))
-        or return '??:??';
-
-    my $seconds = $duration % 60;
-    $duration = ($duration - $seconds) / 60;
-    my $minutes = $duration % 60;
-    $duration = ($duration - $minutes) / 60;
-    my $hours = $duration;
-
-    return sprintf '%d:%02d:%02d', $hours, $minutes, $seconds
-        if $hours > 0;
-
-    return sprintf '%02d:%02d', $minutes, $seconds;
-
 }
 
 }
