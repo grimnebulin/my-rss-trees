6d547d2475cd8826a70b138393d29b2876214594
Sean McAfee <eefacm@gmail.com>
2017-08-15 00:22:38 -0700

    Update Slog feed; clean invalid character numbers

--- Slog.pm
+++ Slog.pm
@@ -4,7 +4,7 @@ use parent qw(BasicFeed RSS::Tree);
 use strict;
 
 use constant {
-    FEED    => 'http://feeds.thestranger.com/stranger/slog',
+    FEED    => 'http://www.thestranger.com/seattle/Rss.xml',
     NAME    => 'slog',
     TITLE   => 'Savage Slog',
     CONTENT => [ '//div[%s]', 'postBody' ],
@@ -13,6 +13,16 @@ use constant {
 
 sub init {
     shift->match_creator('savage');
+}
+
+sub clean_feed {
+    $_[1] =~ s/(&#x([0-9a-fA-F]+);)/_valid_char_hex($2) ? $1 : ""/ge;
+}
+
+sub _valid_char_hex {
+    my $char = shift;
+    my $ord  = hex $char;
+    return $ord == 0x9 || $ord == 0xA || $ord == 0xD || $ord > 0x20;
 }
 
 
